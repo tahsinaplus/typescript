d64d1bb38f9c34a5cb2431463e6f9bc77734df65
tahsinaplus <tahsinaplus@gmail.com>
2023-06-04 08:14:33 -0400

    more inheritance classes

---
+++ inheritance/Rectangle.js
@@ -0,0 +1,55 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+exports.__esModule = true;
+exports.Rectangle = void 0;
+var Shape_1 = require("./Shape");
+var Rectangle = /** @class */ (function (_super) {
+    __extends(Rectangle, _super);
+    function Rectangle(thex, they, _width, _height) {
+        var _this = _super.call(this, thex, they) || this;
+        _this.thex = thex;
+        _this.they = they;
+        _this._width = _width;
+        _this._height = _height;
+        return _this;
+    }
+    Object.defineProperty(Rectangle.prototype, "height", {
+        get: function () {
+            return this._height;
+        },
+        set: function (value) {
+            this._height = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Rectangle.prototype, "width", {
+        get: function () {
+            return this._width;
+        },
+        set: function (value) {
+            this._width = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Rectangle.prototype.getInfo = function () {
+        return _super.prototype.getInfo.call(this) + ", width = ".concat(this._width, ", height = ").concat(this._height);
+    };
+    return Rectangle;
+}(Shape_1.Shape));
+exports.Rectangle = Rectangle;
